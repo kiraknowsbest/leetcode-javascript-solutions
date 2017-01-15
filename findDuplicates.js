75aa9ef6383ce2b15687c867366432a80e0129ae
Daniel Metzger <kinaharo@gmail.com>
2017-01-15 05:13:49 -0800

    Add findDuplicates solution

---
+++ findDuplicates.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+const findDuplicates = nums => {
+  let duplicates = [];
+  for (let i = 0; i < nums.length; i++) {
+    let index = Math.abs(nums[i])-1;
+    if (nums[index] < 0) duplicates.push(Math.abs(index+1));
+    nums[index] = -nums[index];
+  }
+  return duplicates;
+};
