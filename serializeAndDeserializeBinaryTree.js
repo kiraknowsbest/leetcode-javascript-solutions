f35c0763d0c06e5d794ff8fe0baa1d025bc67cda
Daniel Metzger <kinaharo@gmail.com>
2017-01-16 04:58:12 -0800

    Add serialize and deserialize binary tree solution

---
+++ serializeAndDeserializeBinaryTree.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+const serialize = (root) => {
+    if (!root) return [];
+    let data = [];
+    let frontier = [root];
+    while(frontier.length > 0) {
+        let node = frontier.shift();
+        data.push(node);
+        data.push(node.left);
+        data.push(node.right);
+        if (node.left) frontier.push(node.left);
+        if (node.right) frontier.push(node.right);
+    }
+    return data;
+};
+
+const deserialize = data => {
+    let root = data[0] || null;
+    while(data.length > 0) {
+        let node = new TreeNode(data.shift());
+        node.left = data[0];
+        node.right = data[1];
+    }
+    return root;
+};
