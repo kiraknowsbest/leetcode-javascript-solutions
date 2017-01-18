1141c1167d6324170f07c2174303bb4855f6fe35
Daniel Metzger <kinaharo@gmail.com>
2017-01-18 01:49:06 -0800

    Add validParentheses solution

---
+++ validParentheses.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/valid-parentheses/
+const isValid = s => {
+    let stack = [];
+    let mapping = {
+        '{': '}',
+        '[':']',
+        '(':')'
+    };
+    for (let i = 0; i < s.length; i++) {
+        let cur = s[i];
+        if (mapping[cur]) {
+            stack.push(cur);
+        } else {
+            if (mapping[stack.pop()] !== cur) return false;
+        }
+    }
+    return stack.length === 0;
+};
