b7fee9ba6f39e9038ec4f1a3fa25079b77c478b6
Daniel Metzger <kinaharo@gmail.com>
2021-02-25 00:52:31 -0800

    Fix condition

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/palindrome-number/description/
 const isPalindrome = x => {
   let a = x.toString();
-  for (let i = 0, j = a.length - 1; i <= j; i++, j--) {
+  for (let i = 0, j = a.length - 1; i < j; i++, j--) {
     if (a[i] !== a[j]) return false;
   }
   return true;
